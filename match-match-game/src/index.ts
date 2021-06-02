e5b550e53c32294263e9a80c4f52d7af981bcb10
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-06-02 17:51:01 +0300

    refactor: make addApp IIFE

--- match-match-game/src/index.ts
+++ match-match-game/src/index.ts
@@ -2,15 +2,13 @@ import './scss/app.scss';
 
 import App from './app/app';
 
-const addApp = () => {
+(function addApp() {
   const rootElem = document.getElementById('app');
 
   if (!rootElem) throw new Error('There is no element with "app" id');
 
   const app = new App(rootElem);
   app.render();
-};
-
-addApp();
+})();
 
 // indexedDB.deleteDatabase('foggylight');
