b100f76894c94ff7835a02c5c6122d67ece19520
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-06-10 06:29:57 +0300

    feat: add links in header

--- async-race/src/components/header.ts
+++ async-race/src/components/header.ts
@@ -1,11 +1,37 @@
+import state from '../state';
 import Component from './component';
 
 export default class Header extends Component {
+  links: HTMLElement[];
+
   constructor(parentNode: HTMLElement) {
     super(parentNode, 'header', ['page-header']);
+    this.links = [];
+
+    const list = new Component(this.node, 'ul').node;
+    const pagesList = [
+      { name: 'Garage', path: '/' },
+      { name: 'Winners', path: '/winners' },
+    ];
+
+    pagesList.forEach(page => {
+      const listItem = new Component(list, 'li', ['header__list-item']).node;
+      const link = new Component(listItem, 'a', ['navigation__link'], page.name)
+        .node;
+      if (state.currentRoute === page.path) {
+        link.classList.add('navigation__link_active');
+      }
+      link.setAttribute('href', `#${page.path.slice(1)}`);
+      this.links.push(link);
+    });
   }
 
   render(): void {
-    this.node.textContent = 'header';
+    this.links.forEach(link => {
+      link.classList.remove('navigation__link_active');
+      if (state.currentRoute === `/${link.getAttribute('href')?.slice(1)}`) {
+        link.classList.add('navigation__link_active');
+      }
+    });
   }
 }
